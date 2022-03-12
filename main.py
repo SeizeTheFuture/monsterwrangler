4c370ce9f2c2dd18ab1181fec25e76988c1c219e
Matt Gottilla <matthew.gottilla@gmail.com>
2022-03-12 11:49:40 -0800

    Player and Monster Classes built, Partial Game Class completion

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 
 #Initialize pygame
@@ -16,17 +17,99 @@ clock = pygame.time.Clock()
 #Define Classes
 class Game():
     """A class to control gameplay"""
-    def __init__(self):
+    def __init__(self, player, monster_group):
         """Initialize the game object"""
-        pass
+        #Set game values
+        self.score = 0
+        self.round_number = 0
+
+        self.round_time = 0
+        self.frame_count = 0
+
+        self.player = player
+        self.monster_group = monster_group
+
+        #Set sounds and music
+        self.next_level_sound = pygame.mixer.Sound("next_level.wav")
+
+        #Set font
+        self.font = pygame.font.Font("Abrushow.ttf", 24)
+
+        #Set images
+        blue_image = pygame.image.load("blue_monster.png")
+        green_image = pygame.image.load("green_monster.png")
+        purple_image = pygame.image.load("purple_monster.png")
+        yellow_image = pygame.image.load("yellow_monster.png")
+        #This list corresponds to the monster_type attribute, 0 -> blue, 1 -> green, 2 -> purple, 3 -> yellow
+        self.target_monster_images = [blue_image, green_image, purple_image, yellow_image]
+
+        self.target_monster_type = random.randint(0,3)
+        self.target_monster_image = self.target_monster_images[self.target_monster_type]
+
+        self.target_monster_rect = self.target_monster_image.get_rect()
+        self.target_monster_rect.centerx = WINDOW_WIDTH//2
+        self.target_monster_rect.top = 30
 
     def update(self):
         """Update our game object"""
-        pass
+        self.frame_count +=1
+        if self.frame_count == FPS:
+            self.round_time += 1
+            self.frame_count = 0
+
+        #Check collisions
+        self.check_collisions()
 
     def draw(self):
         """Draw the HUD and other things to the display"""
-        pass
+        #Set colors
+        WHITE = (255,255,255)
+        BLUE = (20, 176, 235)
+        GREEN = (87, 201, 47)
+        PURPLE = (226, 73, 243)
+        YELLOW = (243, 157, 20)
+
+        #Add the monster colors to a list where the index of the color matches the index of the target monster images
+        colors = [BLUE, GREEN, PURPLE, YELLOW]
+
+        #Set text
+        catch_text = self.font.render("Current Catch", True, WHITE)
+        catch_rect = catch_text.get_rect()
+        catch_rect.centerx = WINDOW_WIDTH//2
+        catch_rect.top = 5
+
+        score_text = self.font.render("Score: " + str(self.score), True, WHITE)
+        score_rect = score_text.get_rect()
+        score_rect.topleft = (5,5)
+
+        lives_text = self.font.render("Lives: " + str(self.player.lives), True, WHITE)
+        lives_rect = lives_text.get_rect()
+        lives_rect.topleft = (5, 35)
+
+        round_text = self.font.render("Current Round: " + str(self.round_number), True, WHITE)
+        round_rect = round_text.get_rect()
+        round_rect.topleft = (5, 65)
+
+        time_text = self.font.render("Round Time: " + str(self.round_time), True, WHITE)
+        time_rect = time_text.get_rect()
+        time_rect.topright = (WINDOW_WIDTH - 10, 5)
+
+        warp_text = self.font.render("Warps: " + str(self.player.warps), True, WHITE)
+        warp_rect = warp_text.get_rect()
+        warp_rect.topright = (WINDOW_WIDTH - 10, 35)
+
+        #Blit the HUD
+        display_surface.blit(catch_text, catch_rect)
+        display_surface.blit(score_text, score_rect)
+        display_surface.blit(round_text, round_rect)
+        display_surface.blit(lives_text, lives_rect)
+        display_surface.blit(time_text, time_rect)
+        display_surface.blit(warp_text, warp_rect)
+        display_surface.blit(self.target_monster_image, self.target_monster_rect)
+
+        #Draw a rectangle that's the same color as target monster
+        pygame.draw.rect(display_surface, colors[self.target_monster_type], (WINDOW_WIDTH//2 - 32, 30, 64, 64), 2)
+        pygame.draw.rect(display_surface, colors[self.target_monster_type], (0, 100, WINDOW_WIDTH, WINDOW_HEIGHT - 200), 4)
 
     def check_collisions(self):
         """Check for collisions between players and monsters"""
@@ -52,30 +135,89 @@ class Player(pygame.sprite.Sprite):
     """A player class that the user can control"""
     def __init__(self):
         """Initialize the player"""
-        pass
+        super().__init__()
+        self.image = pygame.image.load("knight.png")
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WINDOW_WIDTH//2
+        self.rect.bottom = WINDOW_HEIGHT
+
+        self.lives = 5
+        self.warps = 2
+        self.velocity = 8
+
+        self.catch_sound = pygame.mixer.Sound("catch.wav")
+        self.warp_sound = pygame.mixer.Sound("warp.wav")
+        self.die_sound = pygame.mixer.Sound("die.wav")
 
     def update(self):
         """Update the player"""
-        pass
+        keys = pygame.key.get_pressed()
+
+        #Move the player within the bounds of the screen
+        if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and self.rect.left > 0:
+            self.rect.x -= self.velocity
+        if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and self.rect.right < WINDOW_WIDTH:
+            self.rect.x += self.velocity
+        if (keys[pygame.K_UP] or keys[pygame.K_w]) and self.rect.top > 100:
+            self.rect.y -= self.velocity
+        if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and self.rect.bottom < WINDOW_HEIGHT:
+            self.rect.y += self.velocity
 
     def warp(self):
         """Warps the player to safety at the bottom of the screen"""
-        pass
+        if self.warps > 0:
+            self.warps -= 1
+            self.warp_sound.play()
+            self.rect.bottom = WINDOW_HEIGHT
 
     def reset_player(self):
         """Reset the player to the original starting position"""
-        pass
+        self.rect.centerx = WINDOW_WIDTH//2
+        self.rect.bottom = WINDOW_HEIGHT
 
 class Monster(pygame.sprite.Sprite):
     """A class to create enemy monster objects"""
-    def __init__(self):
+    def __init__(self, x, y, image, monster_type):
         """Initialize the monster"""
-        pass
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+
+        #Monster type is an int 0 -> blue, 1 -> green, 2 -> purple, 3 -> yellow
+        self.type = monster_type
+
+        #Set random motion
+        self.dx = random.choice([-1, 1])
+        self.dy = random.choice([-1, 1])
+        self.velocity = random.randint(1,5)
 
     def update(self):
         """Update the monster"""
-        pass
+        self.rect.x += self.dx * self.velocity
+        self.rect.y += self.dy * self.velocity
 
+        #Bounce the monster off the edges of the display
+        if self.rect.left <= 0 or self.rect.right >= WINDOW_WIDTH:
+            self.dx *= -1
+        if self.rect.top <= 0 or self.rect.bottom >= WINDOW_HEIGHT:
+            self.dy *= -1
+
+#Create a player group and player object
+player_group = pygame.sprite.Group()
+player = Player()
+player_group.add(player)
+
+#Create a monster group. Monsters will be added by start_new_round method.
+monster_group = pygame.sprite.Group()
+#Test Monsters
+monster = Monster(300,300,pygame.image.load("green_monster.png"), 1)
+monster_group.add(monster)
+monster = Monster(100,500,pygame.image.load("yellow_monster.png"), 3)
+monster_group.add(monster)
+
+#Create a game object
+game = Game(player, monster_group)
 
 #The main game loop
 running = True
@@ -85,6 +227,20 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    #Fill the display
+    display_surface.fill((0,0,0))
+
+    #Update and draw sprite groups
+    player_group.update()
+    player_group.draw(display_surface)
+
+    monster_group.update()
+    monster_group.draw(display_surface)
+
+    #Update and draw the game
+    game.update()
+    game.draw()
 
     #Update display and tick clock
     pygame.display.update()
